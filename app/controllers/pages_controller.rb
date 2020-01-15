42fd600c13951325a8e6dc60ab6df65fb768632e
Argiris <anastasakis.an@gmail.com>
2020-01-16 01:06:14 +0200

    Add featured users functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @members = User.members.page(params[:page]).per(10)
+    @members = User.members.order(featured: :asc).page(params[:page]).per(10)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_validation :custom_validation, if: :more_than_five_featured_members?
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
@@ -6,4 +7,15 @@ class User < ApplicationRecord
   acts_as_paranoid
 
   scope :members, -> { where.not(confirmed_at: nil) }
+  scope :featured, -> { where(featured: true) }
+
+  def more_than_five_featured_members?
+    User.featured.size > 5
+  end
+
+  private 
+  def custom_validation
+    errors.add :user, "Featured members can not be more than 5"
+    false  
+  end
 end
