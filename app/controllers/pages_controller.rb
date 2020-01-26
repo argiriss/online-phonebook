354a315b057ce70f5653333f466b772dbe2819f5
Argiris <anastasakis.an@gmail.com>
2020-01-26 11:52:21 +0200

    Style featured members

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @members = User.members.order(featured: :asc).page(params[:page]).per(10)
+    @members = User.members.order(featured: :desc).page(params[:page]).per(6)
   end
 end
